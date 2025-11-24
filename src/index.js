ce4124c49269a6d9f7613f0329958c4c645f0bc1
Jose Villar <jaydvillar@gmail.com>
2025-11-24 15:58:09 +0100

    Lab 1 day 1 w2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,57 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description){
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+    getFormattedAmount(){ return this.amount + " €";
+
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description){
+        super(date, amount, description)
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid){
+super(date, amount, description)
+this.type = "expense"
+this.paid = paid 
+    }
+    getFormattedAmount(){
+    return "-" + this.amount + " €"
+    }
+
+}
+
 
 // Budget
-class Budget {}
+class Budget extends Entry {
+    constructor(){
+        super()
+        this.entries = []
+    }
+addEntry(entry){
+    this.entries.push(entry)
+    
+}
+getCurrentBalance(){
+        let balance = 0;
+        for(let i = 0; i < this.entries.length; i++){
+            if (this.entries[i].type === "income"){
+                balance += this.entries[i].amount;
+
+            }else {
+                balance -= this.entries[i].amount;
+            }
+        }
+        return balance;
+    }
+}
